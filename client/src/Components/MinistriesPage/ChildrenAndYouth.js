ef3cc3597fdc07cf1391750c88bce6e961447545
Bekam <mspbekan@gmail.com>
2023-10-30 19:26:16 -0500

    code added to the ChildrenAndYouth pages

--- client/src/Components/MinistriesPage/ChildrenAndYouth.js
+++ client/src/Components/MinistriesPage/ChildrenAndYouth.js
@@ -17,6 +17,11 @@ export default function ChildrenAndYouth() {
       backgroundImage: `url('/images/childYouthPhotos/language.jpg')`,
       backgroundImageRepeat: "no-repeat",
     },
+    cardSize: {
+      width: "35rem",
+      backgroundColor: "yellow",
+      fontWeight: "bold",
+    },
   };
 
   const [displaypage, setDisplayPage] = useState("");
@@ -28,21 +33,87 @@ export default function ChildrenAndYouth() {
     if (displaypage === "sunday school") {
       return (
         <div className="eachRendered-div" style={style.sundayBgrd}>
-          <div className="container">
-            display here the sunday school program
+          <div className="container d-flex justify-content-center align-items-center flex-wrap pt-2">
+            <div class="card bodyCard text-center animate__hinge">
+              <img
+                src="/images/childYouthPhotos/happy-sunday.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Kids Sunday School is led by qualified and experienced
+                  teachers.
+                </p>
+                <p class="card-text">Please send your your kids to join us!</p>
+              </div>
+            </div>
+          </div>
+          <div className="container message-div text-center d-flex justify-content-center align-items-center flex-wrap mt-2 w-50">
+            <p>
+              Jesus said, “Let the little children come to me, and do not hinder
+              them, for the kingdom of heaven belongs to such as these.” Matthew
+              19:14
+            </p>
           </div>
         </div>
       );
     } else if (displaypage === "bible") {
       return (
         <div className="eachRendered-div" style={style.bibleBgrd}>
-          <div className="container">display bible here......</div>
+          <div className="container d-flex justify-content-center align-items-center flex-wrap pt-2">
+            <div
+              class="card bodyCard text-center animate__hinge"
+              style={style.cardSize}
+            >
+              <img
+                src="/images/childYouthPhotos/childrenReading3.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body text-black">
+                <p class="card-text">
+                  We meet every Saturday for bible study and prayer from{" "}
+                  <strong>
+                    <em>11:00 AM – 12:30 PM</em>
+                  </strong>
+                  .
+                </p>
+                <p class="card-text">
+                  Please send your your kids and teenagers to join us!
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       );
     } else if (displaypage === "afan oromo language") {
       return (
         <div className="eachRendered-div" style={style.afanOromoBgrd}>
-          <div className="container">Afan oromo here.......</div>
+          <div className="container d-flex justify-content-center align-items-center flex-wrap pt-2">
+            <div
+              class="card bodyCard text-center animate__hinge"
+              style={style.cardSize}
+            >
+              <img
+                src="/images/childYouthPhotos/childSpeaking.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body text-black">
+                <p class="card-text">
+                  Bible and prayer class followed by
+                  <strong>
+                    <em> Afaan Oromo Language class</em>
+                  </strong>
+                  .
+                </p>
+                <p class="card-text">
+                  Our Kids take part in all church services once a month!
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       );
     } else {
